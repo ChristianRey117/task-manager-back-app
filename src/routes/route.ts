7f1ed3093af11ebe9369d64547403d497ca9c7f2
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-06-27 11:00:35 -0400

    feat(routes): add middleware to task routes

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import taskRoutes from "./task.route";
 import userRoutes from "./user.route";
+import { authenticateJWT } from "../middleware/auth.middleware";
 
 const router = Router();
 
-router.use("/task", taskRoutes);
+router.use("/task", authenticateJWT, taskRoutes);
 router.use("/user", userRoutes);
 
 export default router;
