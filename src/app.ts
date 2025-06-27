cd1a03b099b1f5a74e097891348c7603a90bae86
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-06-27 02:26:41 -0400

    feat(cors): add cors as dependecy and use in app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 import db from "./database/database";
 import routes from "./routes/route";
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/", routes);
 
