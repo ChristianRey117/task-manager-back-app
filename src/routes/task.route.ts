69251a2a00cb8053d7e75ca1bcfd94c1aae5e534
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-06-27 01:44:19 -0400

    feat(middleware): add schema from zod to validate data

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import taskController from "../controllers/task.controller";
+import { taskMiddleware } from "../middleware/task.middleware";
 
 const taskRouter = Router();
 
 taskRouter.get("/", taskController.getAllTask);
 taskRouter.get("/:id", taskController.getTaskById);
-taskRouter.post("/", taskController.postTask);
-taskRouter.put("/:id", taskController.putTask);
+taskRouter.post("/", taskMiddleware, taskController.postTask);
+taskRouter.put("/:id", taskMiddleware, taskController.putTask);
 taskRouter.delete("/:id", taskController.deleteTask);
 
 export default taskRouter;
